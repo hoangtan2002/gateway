bea7f6154e62b1d1511d36aa68a5947c2c1528cc
tan <115695685+hoangtan2002@users.noreply.github.com>
2023-04-01 09:00:09 +0700

    Add unit test

--- unittest/unittest.py
+++ unittest/unittest.py
@@ -1,2 +1,12 @@
 import unittest
+import time
+from MQTT import *
+from uart import *
 
+def uartTest():
+    print("Plug in MCU and chmod now")
+    time.sleep(15)
+    connectAttemp()
+    writeData('!RST#')
+    state = readSerial()
+    print(state)
